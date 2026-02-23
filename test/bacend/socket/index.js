2d9f2fada7f35ebf684356c308a37aa1a224eb8b
pragyann007 <pragyanthapaliya027@gmail.com>
2026-02-23 18:55:56 +0545

    match making backend brainstorming

--- test/bacend/socket/index.js
+++ test/bacend/socket/index.js
@@ -3,12 +3,68 @@ import { io } from "../server.js"
 export const socketFunc = ()=>{
 
     // random match features
-    
+
+    const waitingQuee = [];
+    // waitingQuee = [ "sadikshya"  ]
+    const activePairs = new Map();
+
+io.on('connection', (socket) => {
+        console.log('A user connected:', socket.id);
+
+        socket.on("find-match",()=>{
+            if(waitingQuee.length>0){
+
+                const partnerId = waitingQuee.shift();
+                // waitingQuee = ["hell"  ]-> partnerId = "hell"  -> waitingQuee = []
+
+                const partnerSocket = io.sockets.sockets.get(partnerId);
+
+                if(!partnerSocket){
+                    socket.emit("find-match");
+                    return;
+                }
+
+                const roomId = `room-${socket.id}-${partnerId}`;
+
+                socket.join(roomId);
+                partnerSocket.join(roomId);
+
+                activePairs.set(socket.id,partnerId);
+                activePairs.set(partnerId,socket.id);
+
+                partnerSocket.emit("matched",{room:roomId,initiator:true});
+
+                socket.emit("matched",{room:roomId,initiator:false});
+
+                // testingg onlyyy
 
 
 
-    io.on('connection', (socket) => {
-        console.log('A user connected:', socket.id)
+           
+                
+             
+                
+               
+             
+                
+
+
+
+
+            }
+            else{
+                waitingQuee.push(socket.id);
+                socket.emit("waiting");
+            }
+
+        })
+
+        socket.on("offer",({offer,room})=>{
+            socket.to(room).emit("offer",{offer,room});
+        })
+       
+
+
     })
 
 }
